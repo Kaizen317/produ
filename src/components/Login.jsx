5c0fece200e5924f5b76cdb1f18db2beed0fe9e3
Juan Valdez <Kaizeningenio@gmail.com>
2024-12-16 18:00:01 -0300

    Prueba despliegue automático con GitHub Actions3

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,7 +4,7 @@ const Login = () => {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <div className="w-full max-w-md p-8 space-y-8 bg-white shadow-lg rounded-lg">
-        <h2 className="text-2xl font-bold text-center text-gray-800">Welcome-SAIT</h2>
+        <h2 className="text-2xl font-bold text-center text-gray-800">Welcome-SAIT2</h2>
         <form className="mt-8 space-y-6">
           <div>
             <label htmlFor="email" className="block text-sm font-medium text-gray-700">
